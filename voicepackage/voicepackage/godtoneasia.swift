34782d9185113011bb7aeced1f9fce48a70034d8
b1129012 <88619256+WordHanDa@users.noreply.github.com>
2023-06-04 16:52:54 +0800

    Update godtoneasia.swift

--- voicepackage/voicepackage/godtoneasia.swift
+++ voicepackage/voicepackage/godtoneasia.swift
@@ -14,6 +14,19 @@ class godtoneasia: UIViewController {
         super.viewDidLoad()
 
     }
+    var audio: AVAudioPlayer?
+    let filePath = ["一定要惹別人火是不是阿", "你不要再滾了過來x2", "好啦x3"]
+    //var url = [URL]()
+    let urlY = Bundle.main.url(forResource: "一定要惹別人火是不是阿", withExtension: "mp3")!
+    let urlN = Bundle.main.url(forResource: "你不要再滾了過來x2", withExtension: "mp3")!
+    let urlH = Bundle.main.url(forResource: "好啦x3", withExtension: "mp3")!
+    let urlW = Bundle.main.url(forResource: "我現在在你的家門外", withExtension: "mp3")!
+    let urlTin = Bundle.main.url(forResource: "聽不懂中文是不是阿你", withExtension: "mp3")!
+    let urlCow = Bundle.main.url(forResource: "靠邀", withExtension: "mp3")!
+    let urlLi = Bundle.main.url(forResource: "來拉切戳拉", withExtension: "mp3")!
+    let urlGan = Bundle.main.url(forResource: "幹你到底到底balabala", withExtension: "mp3")!
+    var godtoneasiaArr = [Niyidin,Nibuyao,Haola,Wosandi,tinbudon,Coeyao,LineLa]
+    
     @IBAction func Niyidin(_ sender: UIButton) {
         audio(filePath: urlY)
     }
@@ -76,15 +89,7 @@ class godtoneasia: UIViewController {
     @IBAction func share8(_ sender: UIButton) {
         sharefunc(filePath: urlGan)
     }
-    var audio: AVAudioPlayer?
-    let urlY = Bundle.main.url(forResource: "一定要惹別人火是不是阿", withExtension: "mp3")!
-    let urlN = Bundle.main.url(forResource: "你不要再滾了過來x2", withExtension: "mp3")!
-    let urlH = Bundle.main.url(forResource: "好啦x3", withExtension: "mp3")!
-    let urlW = Bundle.main.url(forResource: "我現在在你的家門外", withExtension: "mp3")!
-    let urlTin = Bundle.main.url(forResource: "聽不懂中文是不是阿你", withExtension: "mp3")!
-    let urlCow = Bundle.main.url(forResource: "靠邀", withExtension: "mp3")!
-    let urlLi = Bundle.main.url(forResource: "來拉切戳拉", withExtension: "mp3")!
-    let urlGan = Bundle.main.url(forResource: "幹你到底到底balabala", withExtension: "mp3")!
+    
     func audio(filePath:Any){
         try? AVAudioSession.sharedInstance().setCategory(.playback)
         audio = try? AVAudioPlayer(contentsOf: filePath as! URL)
